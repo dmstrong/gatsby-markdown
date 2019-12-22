779825c745720c97d9381b7ba7ee8f0b319074b9
dmstrong <dmstrong07@gmail.com>
2019-12-22 13:38:23 -0600

    Updates

---
+++ component---src-pages-index-js-c1946469df6c7d3aa4ea.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{RXBc:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return c}));var a=t("q1tI"),l=t.n(a),r=t("Bl7J"),i=t("vrFN");function c(){return l.a.createElement(r.a,null,l.a.createElement(i.a,{title:"Home"}),l.a.createElement("h1",null,"Hi, I'm Derrick"),l.a.createElement("p",null,"Welcome to my new Gatsby site. In this site, I will be creating a blog running on Gatsby.js and Markdown."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-c1946469df6c7d3aa4ea.js.map
